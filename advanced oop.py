1e36845f88ff22b12b289817547bb35a3c901ee4
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2026-04-03 23:55:22 +0200

    Create advanced oop.py

---
+++ advanced oop.py
@@ -0,0 +1,11 @@
+'''
+
+This chapter assumes that you are familiar with the basics of OOP, so to establish an understanding of common terms, we should agree on the following definitions:
+
+    class — an idea, blueprint, or recipe for an instance;
+    instance — an instantiation of the class; very often used interchangeably with the term 'object';
+    object — Python's representation of data and methods; objects could be aggregates of instances;
+    attribute — any object or class trait; could be a variable or method;
+    method — a function built into a class that is executed on behalf of the class or object; some say that it’s a 'callable attribute';
+    type — refers to the class that was used to instantiate the object.
+'''
